600100e7abecd6ef97b7c64a27b18e5eece2b405
DESKTOP-452479H\felip <felipe.telom1@gmail.com>
2022-06-08 20:35:12 -0300

    errei a url

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'https://quiz-qe8uk1ltj-felipe1920.vercel.app/api'
+const BASE_URL = 'https://quiz-felipe1920.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
